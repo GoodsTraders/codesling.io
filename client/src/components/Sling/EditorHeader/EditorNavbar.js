caac3e1f18fe5625517e549f9791147cd1c0d0c2
Niv Salamon <mnsalamon@gmail.com>
2017-11-14 17:03:16 -0800

    Client for test cases + test case output skeleton set up

--- client/src/components/Sling/EditorHeader/EditorNavbar.js
+++ client/src/components/Sling/EditorHeader/EditorNavbar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 const EditorNavbar = () => (
+
   <nav className="editor-navbar">
     <ul>
-      <li>Item 1</li>
+      <li className="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="dropdown" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+          Dropdown link
+        </a>
+      <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a className="dropdown-item" id='retro-theme'>Retro</a>
+          <a className="dropdown-item" id=''>Comic</a>
+          <a className="dropdown-item" id=''>H4x0r</a>
+          <a className="dropdown-item" id=''>Add More Themes later</a>
+        </div>
+      </li>
       <li>Item 2</li>
       <li>Item 3</li>
     </ul>
   </nav>
+
 );
 
 export default EditorNavbar;
