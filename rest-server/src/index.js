0b591a11942abdf9f82cb38c57bd923dad5229ce
yiytan92 <yiytan@usc.edu>
2017-11-16 16:21:18 -0800

    Update index.js

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -16,9 +16,9 @@ app.use(cors({
 }));
 
 app.use('/api', router);
-app.use(express.static(path.join(__dirname, '../../client/public')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('*',(req,res) => {
-  res.sendFile(path.resolve(__dirname, '../../client/public/index.html'))
+  res.sendFile(path.resolve(__dirname, '../../client/build/index.html'))
 })
 
 app.listen(port, () => log(`rest-server listening on port ${port}`));
